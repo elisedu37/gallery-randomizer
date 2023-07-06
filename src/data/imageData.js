ab97103bdc53d7f53fac9b05c2f2730f86c13928
elisedu37 <46353414+elisedu37@users.noreply.github.com>
2023-07-06 16:36:51 +0200

    Add new data

--- src/data/imageData.js
+++ src/data/imageData.js
@@ -1,38 +1,38 @@
 export const imageData = [
   {
     url: 'https://images.pexels.com/photos/1624487/pexels-photo-1624487.jpeg?w=800',
-    alt: 'Image 1 description',
+    alt: 'Poulet',
   },
   {
     url: 'https://images.pexels.com/photos/376464/pexels-photo-376464.jpeg?w=800',
-    alt: 'Image 2 description',
+    alt: 'Pancake',
   },
   {
     url: 'https://images.pexels.com/photos/1099680/pexels-photo-1099680.jpeg?w=800',
-    alt: 'Image 3 description',
+    alt: 'Petit déj',
   },
   {
-    url: 'https://images.pexels.com/photos/1624487/pexels-photo-1624487.jpeg?w=800',
-    alt: 'Image 1 description',
+    url: 'https://images.pexels.com/photos/1633525/pexels-photo-1633525.jpeg?w=800',
+    alt: 'Sandwitch',
   },
   {
-    url: 'https://images.pexels.com/photos/376464/pexels-photo-376464.jpeg?w=800',
-    alt: 'Image 2 description',
+    url: 'https://images.pexels.com/photos/699953/pexels-photo-699953.jpeg?w=800',
+    alt: 'Plat asiatique',
   },
   {
-    url: 'https://images.pexels.com/photos/1099680/pexels-photo-1099680.jpeg?w=800',
-    alt: 'Image 3 description',
+    url: 'https://images.pexels.com/photos/2097090/pexels-photo-2097090.jpeg?w=800',
+    alt: 'Assiete garnie',
   },
   {
-    url: 'https://images.pexels.com/photos/1624487/pexels-photo-1624487.jpeg?w=800',
-    alt: 'Image 1 description',
+    url: 'https://images.pexels.com/photos/1410235/pexels-photo-1410235.jpeg?w=800',
+    alt: 'Légumes et oeufs',
   },
   {
-    url: 'https://images.pexels.com/photos/376464/pexels-photo-376464.jpeg?w=800',
-    alt: 'Image 2 description',
+    url: 'https://images.pexels.com/photos/323682/pexels-photo-323682.jpeg?w=800',
+    alt: 'Plat gastro',
   },
   {
-    url: 'https://images.pexels.com/photos/1099680/pexels-photo-1099680.jpeg?w=800',
-    alt: 'Image 3 description',
+    url: 'https://images.pexels.com/photos/1435735/pexels-photo-1435735.jpeg?w=800',
+    alt: 'Pamplemouses',
   },
 ];
